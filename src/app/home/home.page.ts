2cb9a54739a739851eaae9967176dc1cbdb14ed6
gabriellystefani <125033681+gabriellystefani@users.noreply.github.com>
2023-06-20 18:31:38 +0000

    Atualização Projeto Calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,8 +11,66 @@ import { IonicModule } from '@ionic/angular';
 })
 export class HomePage {
 
-	exibir(numero: number) {
-		console.log(numero);
-	}
+  v1=0;
+  v2=0;
+  numero='number';
+  operador='+'
+  decimal=false;
+  decimals=0;
 
-}
+
+  // Salva e exibe os dados inseridos
+  exibir(n: number) {
+		switch (this.numero) {
+    case 'number':
+      if (this.decimal) {
+       this.decimals++;
+       this.v2 = this.v2 + n * Math.pow(10, -this.decimals);
+        } else {
+          this.v2 = this.v2 * 10 + n;
+        }
+
+      break;
+      case 'operator':
+        this.v2 = n;
+        this.numero = 'number';
+        break;
+      case 'result':
+        this.v1 = 0;
+        this.v2 = n;
+        this.numero = 'number';
+    }
+  }
+
+  // Seleciona os operadores
+  operacao(o: string) {
+    this.calcular();
+    this.operador = o
+    this.v1 = this.v2;
+    this.numero = 'operator';
+  }
+
+  // Mostrar o resultado
+  calcular(){
+    this.v2 = eval('' + this.v1 + this.operador + '(' + this.v2 + ')');
+    this.v1 = 0;
+    this.numero = 'result';
+    this.operador = '+';
+    this.decimal = false;
+    this.decimals = 0;
+  }
+
+  // Adiciona operações com ponto
+  ponto(){
+    this.decimal=true
+  }
+  
+  // Apaga os dados anteriores da calculadora
+  apagar() {
+    this.v2 = 0;
+    this.v1 = 0;
+    this.numero = 'number';
+    this.decimal = false;
+    this.decimals = 0;
+  }
+	}
